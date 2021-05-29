7b01a88cf5083a94e9b89d58e3e8529f0c638205
Csilla <posa.csilla@gmail.com>
2021-05-29 13:35:14 +0200

    Function to display photos
    Created function which will do the following forEach object in the photosArray: creates an <a> element, sets its href and target attributes, creates an <img> element and sets its src, alt and title attributes, puts img inide of a, and both inside the image container div. Also deleted placeholder images.

--- script.js
+++ script.js
@@ -17,12 +17,31 @@ const count = 10;
 const apiKey = 'eEOjgtu82bLyYpwq1NqGaDI_Zh4LvLNxPLN8WZlzkiQ';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//creating elements for links and photos, add to DOM
+function displayPhotos() {
+    //forEach to run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        //creating <a> to link to unspalch
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        //create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        //put <img> inside <a>, then put both inside image container
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
 //Get photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        console.log(photosArray);
+        displayPhotos();
     } catch (error) {
         //catch error
     }
