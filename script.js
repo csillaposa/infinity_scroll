145ae7a6f9130dceb92f699922e83f18577c4dbc
Csilla <posa.csilla@gmail.com>
2021-05-29 13:46:26 +0200

    Helper function for setting attributes
    Created helper function to make it easier and shorter to set attributes.

--- script.js
+++ script.js
@@ -17,19 +17,30 @@ const count = 10;
 const apiKey = 'eEOjgtu82bLyYpwq1NqGaDI_Zh4LvLNxPLN8WZlzkiQ';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 //creating elements for links and photos, add to DOM
 function displayPhotos() {
     //forEach to run function for each object in photosArray
     photosArray.forEach((photo) => {
         //creating <a> to link to unspalch
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         //create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         //put <img> inside <a>, then put both inside image container
         item.appendChild(img);
         imageContainer.appendChild(item);
